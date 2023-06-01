99e826a5bf8be0afb2a8dced1df9599589bbc956
wangyifei9 <1282824691@qq.com>
2023-06-01 17:57:37 +0800

    171查询热门课程与讲师

--- service/service_cmss/src/main/java/com/atguigu/educms/service/impl/CrmBannerServiceImpl.java
+++ service/service_cmss/src/main/java/com/atguigu/educms/service/impl/CrmBannerServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.educms.service.impl;
 import com.atguigu.educms.entity.CrmBanner;
 import com.atguigu.educms.mapper.CrmBannerMapper;
 import com.atguigu.educms.service.CrmBannerService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +19,16 @@ import java.util.List;
  */
 @Service
 public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
-
+    //查询所有banner
     @Override
     public List<CrmBanner> selectAllBanner() {
-        List<CrmBanner> list = baseMapper.selectList(null);
+
+        QueryWrapper<CrmBanner> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("id");
+        //last方法，拼接sql语句
+        wrapper.last("limit 2");
+
+        List<CrmBanner> list = baseMapper.selectList(wrapper);
         return list;
     }
 }
