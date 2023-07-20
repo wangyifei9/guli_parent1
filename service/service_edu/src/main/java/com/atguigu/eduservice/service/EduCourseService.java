7fdc7800225facb8f486e4f1bf78afc8ecd5cd5f
wangyifei9 <1282824691@qq.com>
2023-07-20 17:51:05 +0800

    219课程详情功能接口（2）

--- service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
@@ -1,9 +1,14 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.frontvo.CourseFrontVo;
+import com.atguigu.eduservice.entity.frontvo.CourseWebVo;
 import com.atguigu.eduservice.entity.vo.CourseInfoVo;
 import com.atguigu.eduservice.entity.vo.CoursePublishVo;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -27,4 +32,9 @@ public interface EduCourseService extends IService<EduCourse> {
 
 //删除课程方法
     void removeCourse(String courseId);
+    //1 条件查询带分页查询课程前台
+    Map<String, Object> getCourseFrontList(Page<EduCourse> pageCourse, CourseFrontVo courseFrontVo);
+
+    //根据课程id，编写sql语句查询课程信息
+    CourseWebVo getBaseCourseInfo(String courseId);
 }
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
@@ -1,7 +1,10 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +15,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-02-24
  */
 public interface EduTeacherService extends IService<EduTeacher> {
-
+    //1分页查询讲师的方法
+    Map<String, Object> getTeacherFrontList(Page<EduTeacher> pageTeacher);
 }
