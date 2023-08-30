beeff6e50fb7e552e791e799aad47db329e877c8
wangyifei9 <1282824691@qq.com>
2023-08-30 11:55:00 +0800

    230查询订单接口（完）

--- service/service_order/src/main/java/com/atguigu/eduorder/controller/OrderController.java
+++ service/service_order/src/main/java/com/atguigu/eduorder/controller/OrderController.java
@@ -3,7 +3,9 @@ package com.atguigu.eduorder.controller;
 
 import com.atguigu.commonutils.JwtUtils;
 import com.atguigu.commonutils.R;
+import com.atguigu.eduorder.entity.Order;
 import com.atguigu.eduorder.service.OrderService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +32,15 @@ public class OrderController {
         //创建订单，返回订单号
             String orederNo = orderService.createOrder(courseId, JwtUtils.getMemberIdByJwtToken(request));
             return R.ok().data("orderId",orederNo);
+    }
+    //根据订单id查询订单信息
+    @GetMapping("getOrderInfo/{orderId}")
+    public R getOrderInfo(@PathVariable String orderId ){
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.eq("order_no",orderId);
+        Order order = orderService.getOne(wrapper);
+        return R.ok().data("item",order);
+
     }
 }
 
