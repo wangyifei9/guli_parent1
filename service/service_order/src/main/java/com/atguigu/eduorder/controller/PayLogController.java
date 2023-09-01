8bd47d43aef88047a36e8227cfec76c7bee1834f
wangyifei9 <1282824691@qq.com>
2023-09-01 16:50:16 +0800

    234生成微信二维码接口

--- service/service_order/src/main/java/com/atguigu/eduorder/controller/PayLogController.java
+++ service/service_order/src/main/java/com/atguigu/eduorder/controller/PayLogController.java
@@ -1,9 +1,15 @@
 package com.atguigu.eduorder.controller;
 
 
+import com.atguigu.commonutils.R;
+import com.atguigu.eduorder.service.PayLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -16,6 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduorder/pay-log")
 public class PayLogController {
+    @Autowired
+    private PayLogService payLogService;
+    //生成微信支付二维码接口
+    //参数是订单号
+    @GetMapping("createNative/{orderNo}")
+    public R createNative(@PathVariable String orderNo){
+        //返回信息，包含二维码地址，还有其他信息
+        Map map = payLogService.createNative(orderNo);
+        return R.ok().data(map);
+    }
 
 }
 
--- service/service_order/src/main/java/com/atguigu/eduorder/service/PayLogService.java
+++ service/service_order/src/main/java/com/atguigu/eduorder/service/PayLogService.java
@@ -3,6 +3,8 @@ package com.atguigu.eduorder.service;
 import com.atguigu.eduorder.entity.PayLog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
  * <p>
  * 支付日志表 服务类
@@ -12,5 +14,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-07-28
  */
 public interface PayLogService extends IService<PayLog> {
-
+    //生成微信支付二维码接口
+    Map createNative(String orderNo);
 }
